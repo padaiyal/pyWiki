af27358c4c6ced596558362dbf0eeea48c52d62d
Ranjan Mohan <ranjanmj@gmail.com>
2021-05-13 20:01:45 -0400

    Added an example to compare for loop and list comprehension.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyWiki',
+    version='',
+    packages=['wiki', 'wiki.loops'],
+    url='https://github.com/pyPadaiyal/pyWiki',
+    license='',
+    author='Ranjan Mohan',
+    author_email='',
+    description='',
+    install_requires=['matplotlib']
+)
